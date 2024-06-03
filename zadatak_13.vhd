74ab7492912490fececc6725f10f6337b44fe286
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 19:14:36 +0200

    Create zadatak_13.vhd

---
+++ zadatak_13.vhd
@@ -0,0 +1,3 @@
+-- 25. Za PicoBlaze procesor napisati sljedeći prekidni potprogram. Na svaki zahtjev za prekid potrebno je očitati stanje vanjske jedinice koja je spojena na IN_PORT. 
+-- Na dobivenom podatku ispituje se parnost: ako je paran, potrebno je povećati brojač parnih podataka u ScratchPad memoriji na adresi hex (10),
+-- a ukoliko je neparan, povećati brojač neparnih podataka koji se nalazi u ScratchPad memoriji na adresi hex (11).
