6e8fcd868b4e4b9a9d0eeb541d1e71acc704a7e8
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 19:16:18 +0200

    Create zadatak_14.vhd

---
+++ zadatak_14.vhd
@@ -0,0 +1,5 @@
+-- 26. Napišite dio VHDL koda (potrebno je napisati samo proces) koji služi za spajanje izlaznih vanjskih jedinica na PicoBlaze? Objasnite kako radi?
+-- Napisati primjer korištenja vanjske jedinice u assembleru. 
+-- Nacrtati shematski spajanje na KCPSM3 procesor.
+
+
