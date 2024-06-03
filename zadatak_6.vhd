5ede2f91bba05e271c780530972ba283f5ee3543
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 19:03:20 +0200

    Create zadatak_6.vhd

---
+++ zadatak_6.vhd
@@ -0,0 +1,5 @@
+-- 15. Potrebno je u VHDL‐u projektirati 16‐bitni brojač s asinkronim resetom. 
+-- Brojač ima mogućnost brojanja unaprijed i unatrag što se kontrolira pomoću priključka UP_DOWN (‘1’ – unaprijed, ‘0’‐ unatrag). 
+-- Brojač ima na ulazu signal VALUE širine 4 bita koji definira vrijednost s kojom se brojač uvećava ili smanjuje. 
+-- Brojač na ulazu ima priključak ENABLE koji ako je postavljen u '1' omogućuje brojanje dok u drugom slučaju ga zaustavlja. 
+-- Prilikom pojave asinkronog reseta svi izlazni signali sklopa se postavljaju u vrijednost '0'.
