ae452e052103b6b6f3efa4106bcab77b604d27c6
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-14 14:46:08 +0200

    Update zadatak_9.vhd

--- zadatak_9.vhd
+++ zadatak_9.vhd
@@ -1,3 +1,7 @@
--- 20. Potrebno je u VHDL-u projektirati 10-bitni brojač s asinkronim resetom. Brojač ima mogućnost brojanja unaprijed i unatrag što kontrolira pomoću priključka UP_DOWN ( 1 – naprijed, 0 –natrag).
--- Brojač ima na ulazu signal VALUE (4 bita) koji definira vrijednost za koju se povećava ili smanjuje. Brojač na ulazu ima priključke ENABLE ( 1-enable, 0-disable).
+-- isti kao https://github.com/marci29/urs_ciklus_4/blob/main/zadatak_6.vhd
+
+-- 20. Potrebno je u VHDL-u projektirati 10-bitni brojač s asinkronim resetom. 
+-- Brojač ima mogućnost brojanja unaprijed i unatrag što kontrolira pomoću priključka UP_DOWN ( 1 – naprijed, 0 –natrag).
+-- Brojač ima na ulazu signal VALUE (4 bita) koji definira vrijednost za koju se povećava ili smanjuje. 
+-- Brojač na ulazu ima priključke ENABLE ( 1-enable, 0-disable).
 -- Prilikom pojave asinkronog reseta svi izlazni signali postavljaju se u 0.
