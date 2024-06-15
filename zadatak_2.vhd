2294c1ad849f8b58b26c2d847ccb6c9cd4e77b88
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-15 13:01:15 +0200

    Update zadatak_2.vhd

--- zadatak_2.vhd
+++ zadatak_2.vhd
@@ -1,1 +1,3 @@
+-- isti kao 6. https://github.com/marci29/urs_ciklus_4/blob/main/zadatak_6.vhd
+
 -- 6. U VHDL-u definirajte 16-bitno brojilo s asinkronim resetom (prilikom reseta brojilo se postavlja u nulu). Potrebno je napisati entitet i arhitekturu.
